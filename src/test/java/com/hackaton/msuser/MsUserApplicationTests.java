fcdb7cef668507a2bc56dcf132737ee4b9f60384
medYassineHR <harrachmin.m.yassine@gmail.com>
2022-10-09 01:57:21 +0100

    first commit

---
+++ src/test/java/com/hackaton/msuser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaton.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
