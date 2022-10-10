2d19e5003fa2868d928bc382287a9960fe18cc8e
Koala <70296933+m-yassine-kun@users.noreply.github.com>
2022-10-10 01:28:40 +0100

    Main to master (#1)
    * add entities and auth
    
    * add JWT auth and docker

---
+++ src/main/java/com/hackaton/msuser/dao/AppRoleRepository.java
@@ -0,0 +1,10 @@
+package com.hackaton.msuser.dao;
+
+import com.hackaton.msuser.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
+    public AppRole findByRoleName(String roleName);
+}
